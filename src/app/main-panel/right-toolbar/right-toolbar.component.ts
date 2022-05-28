01298100c1dc33f46b44f8d5484f25c70cfa08cb
Dor Harongi <Dorharongi@gmail.com>
2022-05-28 13:22:59 +0300

    added troops viewing

--- src/app/main-panel/right-toolbar/right-toolbar.component.ts
+++ src/app/main-panel/right-toolbar/right-toolbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from 'src/app/login/login.service';
 
 @Component({
   selector: 'app-right-toolbar',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RightToolbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private loginService: LoginService) { }
+
+  cropProduction!: number;
+  woodProduction!: number;
+  stoneProduction!: number;
+
+  spearFighters!: number;
+  swordFighters!: number;
+  axeFighters!: number;
+  archers!: number;
+  magicians!: number;
+  horsemen!: number;
+  catapults !: number;
+
 
   ngOnInit(): void {
+    this.cropProduction = this.loginService.userInformation.villages[0].cropProductionPerSecond * 3600;
+    this.woodProduction = this.loginService.userInformation.villages[0].woodProductionPerSecond * 3600;
+    this.stoneProduction = this.loginService.userInformation.villages[0].stoneProductionPerSecond * 3600;
+
+    this.spearFighters = this.loginService.userInformation.villages[0].troops.troop1Amount;
+    this.swordFighters = this.loginService.userInformation.villages[0].troops.troop2Amount;
+    this.axeFighters = this.loginService.userInformation.villages[0].troops.troop3Amount;
+    this.archers = this.loginService.userInformation.villages[0].troops.troop4Amount;
+    this.magicians = this.loginService.userInformation.villages[0].troops.troop5Amount;
+    this.horsemen = this.loginService.userInformation.villages[0].troops.troop6Amount;
+    this.catapults = this.loginService.userInformation.villages[0].troops.troop7Amount;
   }
 
 }
