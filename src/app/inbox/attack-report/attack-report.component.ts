1153a427f7e3c9245eeec3e993074a48d9d4854b
Dor Harongi <Dorharongi@gmail.com>
2023-04-02 17:37:31 +0300

    implemented "none of your soldiers returned" in the attack report.

--- src/app/inbox/attack-report/attack-report.component.ts
+++ src/app/inbox/attack-report/attack-report.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { TroopsAmounts } from 'src/app/main-panel/models/troopsAmounts';
+import { UserInformationService } from 'src/app/user-information/user-information.service';
 import { AttackReport } from '../models/attackReport';
 
 @Component({
@@ -12,26 +13,37 @@ export class AttackReportComponent implements OnInit {
   @Input() attackReport!: AttackReport;
   @Output() closed: EventEmitter<any> = new EventEmitter<any>();
   troopDictionariesByType: {[troopsType: string]: TroopsAmounts[]} = {};
+  userAttackedAndLost!: boolean;
 
 
 
-  constructor() { }
+  constructor(private userInformationService: UserInformationService) { }
 
   ngOnInit(): void {
-    this.troopDictionariesByType["attacker"] = [];
-    this.troopDictionariesByType["defender"] = [];
-    this.troopDictionariesByType["support"] = [];
+    this.userAttackedAndLost = this.didUserAttackAndLost(); 
 
+    this.troopDictionariesByType["attacker"] = [];
     this.troopDictionariesByType["attacker"][0] = this.attackReport.attackerTroops;
     this.troopDictionariesByType["attacker"][1] = this.attackReport.attackerLostTroops;
-    this.troopDictionariesByType["defender"][0] = this.attackReport.defenderTotalTroops;
-    this.troopDictionariesByType["defender"][1] = this.attackReport.defenderTotalLostTroops;
-    this.troopDictionariesByType["support"][0] = this.attackReport.supportTotalTroops;
-    this.troopDictionariesByType["support"][1] = this.attackReport.supportTotalLostTroops;
+    if(!this.userAttackedAndLost)
+    {
+      this.troopDictionariesByType["defender"] = [];
+      this.troopDictionariesByType["support"] = [];
+      this.troopDictionariesByType["defender"][0] = this.attackReport.defenderTotalTroops;
+      this.troopDictionariesByType["defender"][1] = this.attackReport.defenderTotalLostTroops;
+      this.troopDictionariesByType["support"][0] = this.attackReport.supportTotalTroops;
+      this.troopDictionariesByType["support"][1] = this.attackReport.supportTotalLostTroops;
+    }
+        
   }
+
 
   goBack(){
     this.closed.emit();
+  }
+
+  didUserAttackAndLost(): boolean{
+    return this.attackReport.attackerName == this.userInformationService.userInformation.username &&!this.attackReport.attackerWon;
   }
 
 }
